6aaedcab1e67e76275443d3ecc519a3b3f5c71c0
Alyssa Gee <a.gee.educator@gmail.com>
2025-08-13 15:21:10 -0700

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+/*
+AP CS A
+Name: Ms. Gee
+Date: August 22, 2025
+Assignment: Project 0 - Hello World!
+Description: Write a program that displays "Hello World!"
+Specifications:
+​- Remember to import all necessary packages. 
+- Comment your code!! 
+*/
+ 
+public class HelloWorld{
+    public static void main(String[] args) {
+ 
+        /* to be implemented */ 
+        
+    }
+}
